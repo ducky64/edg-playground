110d0d53dec81beeb88cddcc3b17ce0e929ab6f7
Richard Lin <elpato25@gmail.com>
2026-09-12 18:49:25 -0700

    shims

--- src/main/python/hdl_server.py
+++ src/main/python/hdl_server.py
@@ -1,1 +1,19 @@
-print("HDL SERVER LOADED")
+from typing import Type
+
+from edg import edgrpc, Block, edgir, builder
+from edg.hdl_server.__main__ import process_request
+
+
+def edgjs_process_request_bytes(request_bytes: bytes) -> bytes:
+    hdl_request = edgrpc.HdlRequest()
+    hdl_request.ParseFromString(request_bytes)
+    hdl_response = process_request(hdl_request)
+    if hdl_response is None:
+        return b""
+    return hdl_response.SerializeToString()
+
+
+def compile_request(block: Type[Block]) -> bytes:
+    block_obj = block()
+    request = edgrpc.CompilerRequest(design=edgir.Design(contents=builder.elaborate_toplevel(block_obj)))
+    request.SerializeToString()
