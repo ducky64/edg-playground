57d8827092d090c1752b3429fa487ab7010bed35
Richard Lin <elpato25@gmail.com>
2026-09-19 19:51:48 -0700

    fix deploy script, add repo link

--- src/main.ts
+++ src/main.ts
@@ -31,6 +31,7 @@ app.innerHTML = `
   </p>
 </div>
 <section id="control">
+  <a href="https://github.com/ducky64/edg-playground">edg-playground</a> v0.0 (preview)
   <button popovertarget="helpPopover">?</button>
   <button type="button" id="run-btn" class="btn btn-success" disabled></button> 
   <button type="button" id="download-netlists-btn" class="btn btn-success" disabled>Download netlist</button> 
